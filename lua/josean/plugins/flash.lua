2ebf7d86cc302530a0345755f0297574e450d36a
EdgarQuinonez <l22410043@itparral.edu.mx>
2026-05-02 20:18:21 -0600

    Updated flash config to implement incremental selection

--- lua/josean/plugins/flash.lua
+++ lua/josean/plugins/flash.lua
@@ -18,6 +18,22 @@ return {
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+
+    -- ✅ NEW: incremental selection
+    {
+      "<c-space>",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter({
+          actions = {
+            ["<c-space>"] = "next", -- expand
+            ["<bs>"] = "prev",      -- shrink
+          },
+        })
+      end,
+      desc = "Treesitter Incremental Selection (Flash)",
+    },
+
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
