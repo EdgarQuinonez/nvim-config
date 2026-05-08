6554e7c07a960ef51a631d396e5dc31bb0d6b388
EdgarQuinonez <l22410043@itparral.edu.mx>
2026-05-08 17:23:12 -0600

    FIX - Telescope preview highlight was not working

--- lua/josean/plugins/flash.lua
+++ lua/josean/plugins/flash.lua
@@ -18,8 +18,7 @@ return {
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-
-    -- ✅ NEW: incremental selection
+    
     {
       "<c-space>",
       mode = { "n", "x", "o" },
