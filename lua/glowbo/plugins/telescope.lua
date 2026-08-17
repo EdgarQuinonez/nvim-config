bcde180f4ce3329420168a43732b65584c483a90
Edgar Quinonez <l22410043@itparral.edu.mx>
2026-08-16 18:20:23 -0600

    feat: update live_grep with --multiline, --vimgrep options

--- lua/glowbo/plugins/telescope.lua
+++ lua/glowbo/plugins/telescope.lua
@@ -39,7 +39,9 @@ return {
     telescope.load_extension("fzf")
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fg", function()
+      builtin.live_grep({ additional_args = { "--multiline", "--vimgrep" } })
+    end, { desc = "Telescope live grep" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
   end,
